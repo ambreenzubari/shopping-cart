e70cbb5a71fc12b40b314101e5d8e596386b6650
Ambreen Zubari <ambreenzubari3ss@gmail.com>
2024-05-25 17:02:20 +0500

    added context in the application

---
+++ src/context/Context.js
@@ -0,0 +1,13 @@
+import React, { createContext } from "react";
+
+const Cart = createContext();
+
+function Context({children}) {
+  return (
+    <Cart.Provider>
+        {children}
+    </Cart.Provider>
+  )
+}
+
+export default Context;
