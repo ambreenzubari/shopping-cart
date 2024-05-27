b6ecf228341e6d4d0455015e1e379aef8842f452
Ambreen Zubari <ambreenzubari3ss@gmail.com>
2024-05-28 01:41:07 +0500

    added faker and context API

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,13 +1,27 @@
-import React, { createContext } from "react";
-
+import { faker } from "@faker-js/faker";
+import React, { createContext, useContext, useReducer } from "react";
+import { cartReducer } from "./Reducer";
 const Cart = createContext();
 
-function Context({children}) {
-  return (
-    <Cart.Provider>
-        {children}
-    </Cart.Provider>
-  )
+function Context({ children }) {
+  const products = [...Array(2)].map(() => ({
+    id: faker.string.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.image.url(),
+    inStock: faker.helpers.arrayElement([0, 3, 5, 6, 7]),
+    fastDelivery: faker.datatype.boolean(),
+    rating: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
+  }));
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: products,
+    cart: [],
+  });
+  return <Cart.Provider value={{ state, dispatch }}>{children}</Cart.Provider>;
 }
 
 export default Context;
+
+export const CartState = () => {
+  return useContext(Cart);
+};
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Context from "./context/Context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Context>
+    <Context> 
       <App />
     </Context>
   </React.StrictMode>
