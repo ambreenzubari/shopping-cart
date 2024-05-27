e8b2b48f1c69d99b9305968668f6a60c0c0209c5
Ambreen Zubari <ambreenzubari3ss@gmail.com>
2024-05-28 03:25:39 +0500

    added custom rating component

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,52 +1,57 @@
-import React from 'react'
-import { Button, Form } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import Rating from "./Rating";
 
 const Filters = () => {
+  const [rate, setRate] = useState(3);
   return (
-    <div className='filters'>
-        <span className='title'> Search products</span>
-        <span>
-            <Form.Check
-                inline
-                label="ascending"
-                name="group1"
-                type="radio"
-                id={"inline-1"}
-            />
-        </span>
-        <span>
-            <Form.Check
-                inline
-                label="descending"
-                name="group1"
-                type="radio"
-                id={"inline-2"}
-            />
-        </span>
-
-        <span>
+    <div className="filters">
+      <span className="title"> Search products</span>
+      <span>
         <Form.Check
-                inline
-                label="Include Out of Stock"
-                name="group1"
-                type="checkbox"
-                id={"inline-3"}
-            />
-        </span>
-
-        <span>
+          inline
+          label="ascending"
+          name="group1"
+          type="radio"
+          id={"inline-1"}
+        />
+      </span>
+      <span>
         <Form.Check
-                inline
-                label="Fast Delivery Only"
-                name="group1"
-                type="checkbox"
-                id={"inline-4"}
-            />
-        </span>
+          inline
+          label="descending"
+          name="group1"
+          type="radio"
+          id={"inline-2"}
+        />
+      </span>
 
-        <Button variant='light'>Clear Filter</Button>
+      <span>
+        <Form.Check
+          inline
+          label="Include Out of Stock"
+          name="group1"
+          type="checkbox"
+          id={"inline-3"}
+        />
+      </span>
+
+      <span>
+        <Form.Check
+          inline
+          label="Fast Delivery Only"
+          name="group1"
+          type="checkbox"
+          id={"inline-4"}
+        />
+      </span>
+      <span>
+        <label className="ratingLabel">Rating:</label>
+        <Rating rating={rate} onClick={(rating)=>setRate(rating)} style={{cursor:"poiner"}}/>
+      </span>
+      <Button variant="light">Clear Filter</Button>
     </div>
-  )
-}
+  );
+};
 
-export default Filters
+export default Filters;
