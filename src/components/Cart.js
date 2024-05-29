7e1166b0594fc640d9c7d57a2d79e063d83512fc
Ambreen Zubari <ambreenzubari3ss@gmail.com>
2024-05-30 02:04:48 +0500

    created cart screen

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Button, Col, Form, Image, ListGroup, Row } from "react-bootstrap";
+import { CartState } from "../context/Context";
+import Rating from "./Rating";
+import { AiFillDelete } from "react-icons/ai";
 
 function Cart() {
+  const {
+    state: { cart },dispatch
+  } = CartState();
+  const [total, setTotal] = useState();
+
+  useEffect(() => {
+    setTotal(
+      cart.reduce((acc, curr) => acc + Number(curr.price) * curr.qty, 0)
+    );
+  }, [cart]);
   return (
-    <div>Cart</div>
-  )
+    <div className="home">
+      <div className="productContainer">
+        <ListGroup>
+          {cart.map((prod) => (
+            <ListGroup.Item key={prod.id}>
+              <Row style={{alignItems:"center"}}>
+                <Col md={2}>
+                  <Image src={prod.image} alt={prod.name} fluid rounded />
+                </Col>
+                <Col md={2}>
+                  <span>{prod.name}</span>
+                </Col>
+                <Col md={2}>
+                  <span>${prod.price.split(".")[0]}</span>
+                </Col>
+                <Col md={2}>
+                  <Rating rating={prod.rating} />
+                </Col>
+                <Col md={2}>
+                  <Form.Control as="select" value={prod.qty}>
+                    {[...Array(prod.inStock).keys()].map((x) => (
+                      <option key={x + 1}>{x + 1}</option>
+                    ))}
+                  </Form.Control>
+                </Col>
+                <Col md={2}>
+                  <AiFillDelete
+                    fontSize={"20px"}
+                    style={{ cursor: "pointer" }}
+                    onClick={() =>
+                      dispatch({
+                        type: "REMOVE_FROM_CART",
+                        payload: prod,
+                      })
+                    }
+                  />
+                </Col>
+              </Row>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </div>
+      <div className="filters summary">
+        <span className="title">Subtotal {cart.length} items</span>
+        <span style={{ fontWeight: 700, fontSize: 20 }}>Total: ${total}</span>
+        <Button disabled={cart.length == 0}>Proceed to Checkout</Button>
+      </div>
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
